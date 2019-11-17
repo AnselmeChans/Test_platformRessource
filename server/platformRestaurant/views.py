5db2bec5335a48c07661aaa69cf3a8972be2c03f
hans <anselmeceril@gmail.com>
2019-11-17 21:09:28 +0100

    add new model

--- server/platformRestaurant/views.py
+++ server/platformRestaurant/views.py
@@ -1,10 +1,26 @@
 from rest_framework import viewsets
 from django.shortcuts import render
 
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Room, Info
+from .serializers import UserSerializer, RoomSerialiser, InfoSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+class RoomViewSet(viewsets.ModelViewSet):
+    '''
+    This comment will be visible in the web interface
+    '''
+    queryset = Room.objects.all()
+    serializer_class = Room
+
+class InfoViewSet(viewsets.ModelViewSet):
+    '''
+    This comment will be visible in the web interface
+    '''
+    queryset = Info.objects.all()
+    serializer_class = Info
+
+
