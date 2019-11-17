ecd6064a54cb8aa9bc1fdf4afe06ccedc88c0ef7
hans <anselmeceril@gmail.com>
2019-11-17 16:57:52 +0100

    add component and router

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,27 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Ressources from './components/pages/Ressources';
+import NotFoundPage from './components/pages/NotFoundPage';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/room" component={Ressources}></Route>
+          <Route path="/contact" component={Contact}></Route>
+          <Route component={NotFoundPage}></Route>
+        </Switch>
+      </BrowserRouter>
+      
       <Footer></Footer>
     </div>
   );
