e599a84145d1ac78a184828f59729c617fab2cee
hans <anselmeceril@gmail.com>
2019-11-17 20:41:26 +0100

    better consumer

--- client/src/components/context.js
+++ client/src/components/context.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { placeInfo, reviews, detailInfo, news} from '../data'
 
 const InfoContext = React.createContext();
 
 // Provider
 export default class InfoProvider extends Component {
+
+    state = {
+        info: placeInfo, 
+        reviews: reviews, 
+        detailInfo: detailInfo, 
+        new: news
+    }
+
     render() {
         return (
             
-            <InfoContext.Provider value="Hello this is the ressource">
+            <InfoContext.Provider value={{
+                info: this.state.placeInfo, 
+                reviews: this.state.reviews, 
+                detailInfo: this.state.detailInfo, 
+                new: this.state.news
+
+            }}>
                 {this.props.children}
             </InfoContext.Provider>
         );
