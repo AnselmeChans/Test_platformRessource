96b01fcfee7065dfc4bc92dd974fd6805782e345
hans <anselmeceril@gmail.com>
2019-11-17 15:39:05 +0100

    add architecture front

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Footer></Footer>
     </div>
   );
 }
